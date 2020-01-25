bd3a292eb53ba284eed8cd29ff6136ada7434a94
Akashdeep Singh Khanuja <60085485+imask22@users.noreply.github.com>
2020-01-25 21:11:16 +0530

    Add files via upload

--- ask/module4/fruit_value.py
+++ ask/module4/fruit_value.py
@@ -1,31 +1,39 @@
-n=int(input("Enter no of batsman : "))
-dict1={}
-Type_of_player=str(input("Type of player : "))
-dict1[Type_of_player]={}
-list2=[]
-while(n):
-    Name_of_player=str(input("Name of Player : "))
-    dict1[Type_of_player][Name_of_player]={}
+#created using try catch
+n=int(input("enter number of fruits "))
+fruit_list=[]
+i=1
+dict21=dict()
+while(n>0):
     
-    Matches=input("Matches : ")
-    Runs=input("Runs : ")
-    Average=input("Average : ")
-    Highest_Score=int(input("Highest_score : "))
-    
-    dict1[Type_of_player][Name_of_player]['Matches']=Matches
-    dict1[Type_of_player][Name_of_player]['Runs']=Runs
-    dict1[Type_of_player][Name_of_player]['Average']=Average
-    dict1[Type_of_player][Name_of_player]['Highest Score']=Highest_Score
-    list2.append([Type_of_player,Name_of_player,Matches,Runs,Average,Highest_Score])
-    n=n-1
-
-count=0
-max1=0
-thisc=0
-for i in list2:
-    if max1<=i[5]:
-        max1=i[5]
-        thisc=int(count)
-    count+=1
-        
-print(list2[thisc]) 
+    d=dict()
+    dict21[i]=d
+    name = input("Enter Fruit name :  ")
+    fruit_list.append(name)
+    d1=dict()
+    d[name] = d1
+    d1['binomial name'] = input('binomial name ')
+    print("name of 3 major producers ", end=" ")
+    l = []
+    l.append(input().split(" "))
+    d1['major producer'] = l
+    d2=dict()
+    d1['nutrients'] = d2
+    d2['carbs'] = int(input("carbs"))
+    d2['protien'] = int(input("protiens"))
+    d2['fat'] = int(input("fat"))
+    i+=1
+    n-=1
+print(dict21)
+ask=[]
+for x in range(i):
+    try:
+        ask.append(dict21[x+1][fruit_list[x]]['nutrients']['protien'])
+    except KeyError:
+    #wont work without it needs try Catch Block
+        print("",end="")
+for k in range(i):
+    try:
+        if max(ask)==dict21[k+1][fruit_list[k]]['nutrients']['protien']:
+            print(fruit_list[k])
+    except KeyError:
+        print(" ",end=" ")
